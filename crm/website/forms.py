ca4fefca27845818baa25bda0a9bac4c802c772e
Matias <matiassanchezabrego@gmail.com>
2024-01-04 22:26:00 -0300

    Records table

--- crm/website/forms.py
+++ crm/website/forms.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
-from .models import Record
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Adress'}))
