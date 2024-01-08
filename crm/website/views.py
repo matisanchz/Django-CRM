856229b7d4dd328b61375482986c30dd014e2d7c
Matias <matiassanchezabrego@gmail.com>
2024-01-08 18:54:07 -0300

    Customer table

--- crm/website/views.py
+++ crm/website/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login,logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
+
     #Check login
     if request.method == 'POST':
         username = request.POST['username']
@@ -17,7 +20,7 @@ def home(request):
             messages.success(request, 'There was an error loggin in')
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 def register_user(request):
     if request.method == 'POST':
